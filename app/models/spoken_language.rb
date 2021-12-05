29cc03d72a2fcd91ba05d8ccb4f1f2b3e308237d
rossbrink <ross.brinkman@chicagobooth.edu>
2021-12-05 08:17:06 +0000

    Provider Edit Permissions Fixed

--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -12,5 +12,5 @@ class SpokenLanguage < ApplicationRecord
   belongs_to(:provider, { :required => true, :class_name => "Provider", :foreign_key => "provider_id", :counter_cache => true })
   belongs_to(:language, { :required => true, :class_name => "Language", :foreign_key => "language_id", :counter_cache => true })
 
-  validates(:language_id, { :uniqueness => true})
+  validates(:language_id, { :uniqueness => { :scope => [:provider_id] }})
 end

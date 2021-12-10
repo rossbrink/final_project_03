88e6266013c3456057950df1b0a9bdaef5eaa482
rossbrink <ross.brinkman@chicagobooth.edu>
2021-12-10 01:35:35 +0000

    asdf

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -93,7 +93,7 @@ class UserAuthenticationController < ApplicationController
     @user.password_confirmation = params.fetch("query_password_confirmation")
     @user.first_name = params.fetch("query_first_name")
     @user.last_name = params.fetch("query_last_name")
-    @user.reviews_count = params.fetch("query_reviews_count")
+    # @user.reviews_count = params.fetch("query_reviews_count")
 
     
     if @user.valid?
